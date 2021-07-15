c6c34d13784c3c44c526c93e991a8998b8f5ebf9
BoDee <nyx0hemir@gmail.com>
2021-07-15 17:16:25 -0600

    finished post model and endpoints

--- models/Post.js
+++ models/Post.js
@@ -33,7 +33,7 @@ Post.init(
     {
         sequelize,
         freezeTableName: true,
-        underscord: true,
+        underscored: true,
         modelName: 'post'
     }
 );
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -14,7 +14,7 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
     User.findOne({
-        attributes: { exclude: ['passwrod'] },
+        attributes: { exclude: ['password'] },
         where: {
           id: req.params.id
         }
