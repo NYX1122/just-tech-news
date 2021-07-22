59e0bbc3f7069d0ea7159343f286f64a93e3ce92
BoDee <nyx0hemir@gmail.com>
2021-07-22 17:40:17 -0600

    finished login feature

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -33,6 +33,11 @@ router.get('/', (req, res) => {
 });
 
 router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+    
     res.render('login');
 });
 
